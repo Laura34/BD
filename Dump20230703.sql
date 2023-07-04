c40f7cffbde70d9847bd4ea5fc07a9a8904185d2
Laura Mejía <49574408+Laura34@users.noreply.github.com>
2023-07-03 22:18:12 -0600

    Update Dump20230703.sql

--- Dump20230703.sql
+++ Dump20230703.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `Final` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `Final`;
 -- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
 --
 -- Host: ls-f77f57c22644e320a00ba7b3cc53804c3dfceb9f.cnqmtkdkm8tk.ca-central-1.rds.amazonaws.com    Database: Final
@@ -15,13 +17,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
-SET @@SESSION.SQL_LOG_BIN= 0;
 
 --
 -- GTID state at the beginning of the backup 
 --
 
-SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
 
 --
 -- Table structure for table `peliculas`
@@ -73,7 +73,6 @@ LOCK TABLES `usuarios` WRITE;
 INSERT INTO `usuarios` VALUES (1,'Andra','Dee','adee0@tripod.com'),(2,'Catharine','Simkiss','csimkiss1@columbia.edu'),(3,'Katrina','Buckerfield','kbuckerfield2@washingtonpost.com'),(4,'Cory','Bearsmore','cbearsmore3@answers.com'),(5,'Ursa','Huws','uhuws4@europa.eu'),(6,'Raleigh','Spataro','rspataro5@nifty.com'),(7,'Claudian','Thews','cthews6@so-net.ne.jp'),(8,'Rockie','Halwood','rhalwood7@apple.com'),(9,'Loren','Bahl','lbahl8@yahoo.com'),(10,'Igor','Simone','isimone9@indiegogo.com'),(11,'Halette','Coils','hcoilsa@ehow.com'),(12,'Jeniece','Huxter','jhuxterb@acquirethisname.com'),(13,'Seline','Tomkys','stomkysc@virginia.edu'),(14,'Bern','Rawle','brawled@rediff.com'),(15,'Phillie','Helmke','phelmkee@mediafire.com'),(16,'Tedman','Redmell','tredmellf@buzzfeed.com'),(17,'Corene','Kincaid','ckincaidg@feedburner.com'),(18,'Nobe','Bispo','nbispoh@arstechnica.com'),(19,'Lauralee','Shere','lsherei@artisteer.com'),(20,'Natale','Whitley','nwhitleyj@ovh.net'),(21,'Hanni','Yole','hyolek@independent.co.uk'),(22,'Bobbette','Giddy','bgiddyl@creativecommons.org'),(23,'Marylin','Iredale','miredalem@bravesites.com'),(24,'Mordecai','Whitfield','mwhitfieldn@rakuten.co.jp'),(25,'Felecia','Franck','ffrancko@wordpress.org'),(26,'Harriot','Clissett','hclissettp@google.ca'),(27,'Bee','O\'Leahy','boleahyq@telegraph.co.uk'),(28,'Colas','Tichelaar','ctichelaarr@twitter.com'),(29,'Elsworth','Waywell','ewaywells@de.vu'),(30,'Yoshiko','Gail','ygailt@archive.org'),(31,'Christyna','Petera','cpeterau@wikispaces.com'),(32,'Janice','Bradneck','jbradneckv@usa.gov'),(33,'Lacey','Greson','lgresonw@hc360.com'),(34,'Julienne','Isaksen','jisaksenx@shutterfly.com'),(35,'Daron','Escale','descaley@cyberchimps.com'),(36,'Imojean','Grelik','igrelikz@hibu.com'),(37,'Etheline','Fuge','efuge10@indiegogo.com'),(38,'Martelle','Kenright','mkenright11@ebay.com'),(39,'Herman','Eagell','heagell12@theglobeandmail.com'),(40,'Elfrida','Keenlyside','ekeenlyside13@jiathis.com'),(41,'Theobald','Krop','tkrop14@upenn.edu'),(42,'Editha','Halsho','ehalsho15@surveymonkey.com'),(43,'Sybilla','Wellstood','swellstood16@cdbaby.com'),(44,'Sandi','Tottman','stottman17@altervista.org'),(45,'Letti','Adenet','ladenet18@creativecommons.org'),(46,'Damita','Ouver','douver19@washington.edu'),(47,'Aimil','Ganniclifft','aganniclifft1a@hud.gov'),(48,'Morissa','Latour','mlatour1b@seesaa.net'),(49,'Mellicent','Lowrie','mlowrie1c@npr.org'),(50,'Cody','Meedendorpe','cmeedendorpe1d@adobe.com'),(51,'Kippar','Scargle','kscargle1e@washington.edu'),(52,'Cristin','Gitting','cgitting1f@ebay.com'),(53,'Ira','Lerwill','ilerwill1g@pagesperso-orange.fr'),(54,'Marillin','Kubelka','mkubelka1h@google.co.jp'),(55,'Jamill','Treadgear','jtreadgear1i@vistaprint.com'),(56,'Lowrance','Phoenix','lphoenix1j@creativecommons.org'),(57,'Willie','Brandrick','wbrandrick1k@engadget.com'),(58,'Mar','Gniewosz','mgniewosz1l@tripadvisor.com'),(59,'Leonidas','Agass','lagass1m@pen.io'),(60,'Hilly','Eberlein','heberlein1n@google.fr'),(61,'Edmon','Dami','edami1o@scientificamerican.com'),(62,'Odille','Selwyne','oselwyne1p@twitter.com'),(63,'Bill','Thurgood','bthurgood1q@virginia.edu'),(64,'Gabrila','Petigrew','gpetigrew1r@fastcompany.com'),(65,'Paolina','Osborn','posborn1s@fotki.com'),(66,'Oona','Rutley','orutley1t@cbslocal.com'),(67,'Grethel','Golbourn','ggolbourn1u@dailymail.co.uk'),(68,'Jens','Ruter','jruter1v@phoca.cz'),(69,'Ardine','Lowensohn','alowensohn1w@europa.eu'),(70,'Isidor','Lantuffe','ilantuffe1x@sbwire.com'),(71,'Benedicto','Edeson','bedeson1y@issuu.com'),(72,'Sandi','Culshew','sculshew1z@sbwire.com'),(73,'Sheilah','Willson','swillson20@china.com.cn'),(74,'Serene','Risso','srisso21@mozilla.org'),(75,'Vasily','Drezzer','vdrezzer22@wunderground.com'),(76,'Desirae','Crank','dcrank23@europa.eu'),(77,'Filbert','Brereton','fbrereton24@msn.com'),(78,'Beverley','Peery','bpeery25@tumblr.com'),(79,'Zack','Brampton','zbrampton26@blogtalkradio.com'),(80,'Pauly','Casali','pcasali27@admin.ch'),(81,'Lutero','Aldrick','laldrick28@google.com.hk'),(82,'Faydra','Batchelour','fbatchelour29@addthis.com'),(83,'Dulcinea','Whyte','dwhyte2a@redcross.org'),(84,'Danielle','Cushelly','dcushelly2b@flavors.me'),(85,'Collin','Emmitt','cemmitt2c@businesswire.com'),(86,'Jamaal','Posten','jposten2d@uiuc.edu'),(87,'Vikki','Chawkley','vchawkley2e@yolasite.com'),(88,'Allix','Bodle','abodle2f@facebook.com'),(89,'Klement','Sparke','ksparke2g@seattletimes.com'),(90,'Cindee','Klimentyonok','cklimentyonok2h@issuu.com'),(91,'Ofella','Domanek','odomanek2i@timesonline.co.uk'),(92,'Miltie','Minnock','mminnock2j@spotify.com'),(93,'Hortensia','Shaul','hshaul2k@soup.io'),(94,'Shanan','Pond','spond2l@wired.com'),(95,'Derby','Panther','dpanther2m@linkedin.com'),(96,'Annabelle','Gagan','agagan2n@harvard.edu'),(97,'Wenonah','Authers','wauthers2o@rakuten.co.jp'),(98,'Ignace','Hallitt','ihallitt2p@bloglines.com'),(99,'Alfredo','Shute','ashute2q@jimdo.com'),(100,'Leah','Kaye','lkaye2r@ocn.ne.jp');
 /*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
 UNLOCK TABLES;
-SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -84,4 +83,4 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-07-03 21:30:04
+-- Dump completed on 2023-07-03 22:07:34
